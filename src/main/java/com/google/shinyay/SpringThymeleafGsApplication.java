d4500d08c624e1e7a6f5e9c0d8cc1930b09aa35c
shinyay <shinya.com@gmail.com>
2021-05-17 18:51:07 +0900

    Initial Commit

---
+++ src/main/java/com/google/shinyay/SpringThymeleafGsApplication.java
@@ -0,0 +1,13 @@
+package com.google.shinyay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafGsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafGsApplication.class, args);
+	}
+
+}
