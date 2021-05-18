02f72c1583d93ec67fbdb03f877f9de6ae49ae9e
shinyay <shinya.com@gmail.com>
2021-05-18 11:37:22 +0900

    Add post and display data

--- src/main/java/com/google/shinyay/controller/HomeController.java
+++ src/main/java/com/google/shinyay/controller/HomeController.java
@@ -3,13 +3,21 @@ package com.google.shinyay.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String homeGet(Model model) {
         model.addAttribute("th_greet", "Hello");
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String homePost(Model model, @RequestBody String name) {
+        model.addAttribute("th_greet", "Hello, "+ name);
         return "home";
     }
 }
