5773d40fe55ba471b46da6741814daac1b094a1f
shinyay <shinya.com@gmail.com>
2021-05-18 09:32:53 +0900

    Add Home Controller

---
+++ src/main/java/com/google/shinyay/controller/HomeController.java
@@ -0,0 +1,4 @@
+package com.google.shinyay.controller;
+
+public class HomeController {
+}
---
+++ src/test/java/com/google/shinyay/HomeControllerTest.java
@@ -0,0 +1,9 @@
+package com.google.shinyay;
+
+import com.google.shinyay.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HomeController.class)
+public class HomeControllerTest {
+}
