5498181c34286e24e654ce174a413800118f0b47
shinyay <shinya.com@gmail.com>
2021-05-18 09:47:24 +0900

    Add context root access test

--- src/test/java/com/google/shinyay/HomeControllerTest.java
+++ src/test/java/com/google/shinyay/HomeControllerTest.java
@@ -2,8 +2,27 @@ package com.google.shinyay;
 
 import com.google.shinyay.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = HomeController.class)
+@AutoConfigureMockMvc
 public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void rootAccessShouldReturnSuccess() throws Exception {
+        mockMvc.perform(get("/")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(status().is2xxSuccessful());
+    }
 }
