f02ea59e0cd0517afa979dfb62464bde0b5a4fdd
shinyay <shinya.com@gmail.com>
2021-06-13 21:46:08 +0900

    Add an attribute from Value annnotation with app properties

--- src/main/java/com/google/shinyay/controller/HomeController.java
+++ src/main/java/com/google/shinyay/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.google.shinyay.controller;
 
 import com.google.shinyay.config.ConfigProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
-    public HomeController(ConfigProperties props) {
 
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/hello")
+    public String helloPage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
     }
+
 }
