506b203c591ba3b33ecfddadfeba5fdb81a7d8fa
shinyay <shinya.com@gmail.com>
2021-06-13 13:55:43 +0900

    Add annotation to scan Config property class

--- src/main/java/com/google/shinyay/SpringThymeleafGsApplication.java
+++ src/main/java/com/google/shinyay/SpringThymeleafGsApplication.java
@@ -2,8 +2,10 @@ package com.google.shinyay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringThymeleafGsApplication {
 
 	public static void main(String[] args) {
