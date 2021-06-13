920efa955f87b9c44600c8ad77f81400279c7bfc
shinyay <shinya.com@gmail.com>
2021-06-13 13:42:57 +0900

    Add Property Configuration Class

--- src/main/java/com/google/shinyay/config/ConfigProperties.java
+++ src/main/java/com/google/shinyay/config/ConfigProperties.java
@@ -6,4 +6,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "app.config")
 public class ConfigProperties {
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
 }
