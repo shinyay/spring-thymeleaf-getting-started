c16c0d2307e4ab9d8bdce4311d5ef188901b6893
shinyay <shinya.com@gmail.com>
2021-06-13 13:55:14 +0900

    Modify configuration annotation

--- src/main/java/com/google/shinyay/config/ConfigProperties.java
+++ src/main/java/com/google/shinyay/config/ConfigProperties.java
@@ -1,9 +1,7 @@
 package com.google.shinyay.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "app.config")
 public class ConfigProperties {
     public String getAuthor() {
