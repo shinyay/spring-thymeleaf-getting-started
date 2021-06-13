83693fa844767bedba4059c82c017dbdc47660eb
shinyay <shinya.com@gmail.com>
2021-06-13 22:18:31 +0900

    Add a custom method for repository

--- src/main/java/com/google/shinyay/repository/BookRepository.java
+++ src/main/java/com/google/shinyay/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.google.shinyay.repository;
 import com.google.shinyay.entity.Book;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findBookByTitle(String title);
 }
