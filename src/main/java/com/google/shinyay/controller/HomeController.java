b86321fcaa2f8e92da19bba849137b0a8b529061
shinyay <shinya.com@gmail.com>
2021-06-13 13:56:10 +0900

    Add constructor injection of Cofig Property class

--- src/main/java/com/google/shinyay/controller/HomeController.java
+++ src/main/java/com/google/shinyay/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.google.shinyay.controller;
 
+import com.google.shinyay.config.ConfigProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    public HomeController(ConfigProperties props) {
+
+    }
 }
