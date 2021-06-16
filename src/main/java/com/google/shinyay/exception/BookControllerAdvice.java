fe8caf06f90e706480be1dd06f0e179fba0e5d15
shinyay <shinya.com@gmail.com>
2021-06-16 09:17:28 +0900

    Add handeling custom exception with Exception Handler

--- src/main/java/com/google/shinyay/exception/BookControllerAdvice.java
+++ src/main/java/com/google/shinyay/exception/BookControllerAdvice.java
@@ -1,16 +1,24 @@
 package com.google.shinyay.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class BookControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({BookNotFoundException.class})
-    public ResponseEntity<Object> handleNotFoundBook() {
-        
+    public ResponseEntity<Object> handleNotFoundBook(Exception e, WebRequest request) {
+        return handleExceptionInternal(
+                e,
+                "The book is not found",
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request);
     }
 
 }
