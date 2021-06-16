1d0fee68f72a01e5383aec52a24096ebfa3021b4
shinyay <shinya.com@gmail.com>
2021-06-16 09:09:04 +0900

    Add ExceptionHandler method

--- src/main/java/com/google/shinyay/exception/BookControllerAdvice.java
+++ src/main/java/com/google/shinyay/exception/BookControllerAdvice.java
@@ -1,7 +1,16 @@
 package com.google.shinyay.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class BookControllerAdvice {
+public class BookControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({BookNotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundBook() {
+        
+    }
+
 }
