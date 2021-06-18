6448457989ca9ac57de192856f3ed6e90a6664db
shinyay <shinya.com@gmail.com>
2021-06-18 09:19:40 +0900

    Add ApplicationContext test

--- src/test/java/com/google/shinyay/SpringContextTests.java
+++ src/test/java/com/google/shinyay/SpringContextTests.java
@@ -1,13 +1,21 @@
 package com.google.shinyay;
 
+import com.google.shinyay.controller.BookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringContextTests {
 
+	@Autowired
+	private BookController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
