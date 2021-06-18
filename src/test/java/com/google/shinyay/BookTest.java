1873976fa77725d046075e7b822bc8149fab1ab1
shinyay <shinya.com@gmail.com>
2021-06-18 22:32:29 +0900

    Add a test for result as OK

--- src/test/java/com/google/shinyay/BookTest.java
+++ src/test/java/com/google/shinyay/BookTest.java
@@ -3,9 +3,12 @@ package com.google.shinyay;
 import com.google.shinyay.entity.Book;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookTest {
 
@@ -24,5 +27,10 @@ public class BookTest {
                 .body(book)
                 .post(BOOK_API);
         return BOOK_API + "/" + response.jsonPath().get("id");
+    }
+
+    @Test
+    public void whenGetAllBooks_thenOk() {
+        assertThat(RestAssured.get(BOOK_API)).isEqualTo(HttpStatus.OK.value());
     }
 }
