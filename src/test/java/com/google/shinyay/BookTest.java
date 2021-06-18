0d1a27479e44ea9224cb0eab62e5fe43a1a30ba2
shinyay <shinya.com@gmail.com>
2021-06-18 22:03:14 +0900

    Add random book data with commons-lang

--- src/test/java/com/google/shinyay/BookTest.java
+++ src/test/java/com/google/shinyay/BookTest.java
@@ -2,11 +2,15 @@ package com.google.shinyay;
 
 import com.google.shinyay.entity.Book;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+
 public class BookTest {
 
     private static final String BOOK_API = "http://localhost:8080/api/v1/books";
     private Book createBooksForTests() {
         Book book = new Book();
+        book.setTitle(randomAlphabetic(10));
+        book.setAuthor(randomAlphabetic(15));
         return book;
     }
 }
